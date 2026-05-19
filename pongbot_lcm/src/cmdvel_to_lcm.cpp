b01f772d11b71a82ca4c6f1755dcd0d1d81fb1da
Kimgyoomin <kimgyoomin1999@gmail.com>
2026-05-19 19:16:34 +0900

    changed error

--- pongbot_lcm/src/cmdvel_to_lcm.cpp
+++ pongbot_lcm/src/cmdvel_to_lcm.cpp
@@ -24,9 +24,9 @@ public:
     // ROS / LCM interface parameters
     // ------------------------------------------------------------------------
     cmd_vel_topic_ = declare_parameter<std::string>("cmd_vel_topic", "/cmd_vel");
-    lcm_channel_ = declare_parameter<std::string>("lcm_channel", "CMD_VEL");
+    lcm_channel_ = declare_parameter<std::string>("lcm_channel", "NAV_CMD_VEL");
     lcm_url_ = declare_parameter<std::string>(
-      "lcm_url", "udpm://239.255.76.67:7667?ttl=1");
+      "lcm_url", "udpm://239.255.76.67:7667?ttl=255");
 
     // ------------------------------------------------------------------------
     // Publish / safety parameters
